0435df429081e1b3116a354e1e3aad3c0a7c2283
kakhasitchinava <k_sichinava@cu.edu.ge>
2016-12-12 22:01:10 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+app.use('/user/:id', function (req, res, next) {
+  console.log('Request URL:', req.originalUrl)
+  next()
+}, function (req, res, next) {
+  console.log('Request Type:', req.method)
+  next()
+})
